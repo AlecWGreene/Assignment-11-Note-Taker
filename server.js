fe176a752fb4fbd7caffcf7dfc0d023706521dd3
LazarusOfSuburbia <alecwgreene@gmail.com>
2020-08-20 12:15:49 -0400

    Added express route to index.html at /

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const path = require("path");
 
 // Server Setup
 // ================================================================
+
+// Create consts
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 3001;
 
 // Configure settings for the server
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +22,11 @@ app.use(express.static('public'));
 
 // Define Routes
 // ================================================================
- 
 
+// Index page
+app.get("/", (arg_request, arg_response) => {
+    arg_response.json(path.join(__dirname, "public/index.html"));
+});
+
+// Listen on the port
 app.listen(PORT, () => { console.log("NoteTaker listening on port " + PORT); });
