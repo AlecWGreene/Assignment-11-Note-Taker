3d7f2e3ccdb60b2e0e48bd6f138128b96ff4a0e4
LazarusOfSuburbia <alecwgreene@gmail.com>
2020-08-21 00:47:26 -0400

    Added server routes, runs on the local machine

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 
 // Create consts
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3090;
 
 // Configure settings for the server
 app.use(express.urlencoded({ extended: true }));
@@ -20,13 +20,42 @@ app.use(express.static('public'));
 
 
 
+// API Handlers
+// ================================================================
+
+/** Retrieves the notes from db.json */
+function getNotes(arg_request, arg_response){
+    arg_response.json(path.join(__dirname, "./db/db.json"));
+}
+
+/** Saves a note to the db.json */
+function postNote(arg_request, arg_response){
+
+}
+
+/** Deletes a note using a passed id */
+function deleteNote(arg_request, arg_response){
+
+}
+
+
 // Define Routes
 // ================================================================
 
-// Index page
+// Index Page
 app.get("/", (arg_request, arg_response) => {
-    arg_response.json(path.join(__dirname, "public/index.html"));
+    arg_response.sendFile(path.join(__dirname, "public/index.html"));
 });
+
+// Note Added Page
+app.get("*", (arg_request, arg_response) => {
+    arg_response.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// API Routes
+app.get("/api/notes", getNotes);
+app.post("/api/notes", postNote);
+app.delete("/api/notes/:id", deleteNote);
 
 // Listen on the port
 app.listen(PORT, () => { console.log("NoteTaker listening on port " + PORT); });
